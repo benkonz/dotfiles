2019d4af27d41e1ae51cfdeaea181f768da4b0f2
Ben Konz <benkonz16@gmail.com>
2019-06-24 15:51:20 -0400

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,6 +1,16 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
-(require 'company)
+
 (setq company-idle-delay 0.2
       company-minimum-prefix-length 3)
-(add-to-list 'auto-mode-alist '("\\.mly$" . tuareg-menhir-mode))
-(add-to-list 'auto-mode-alist '("\\.mll$" . tuareg-mode))
+
+(add-to-list 'auto-mode-alist '("\\.mll\\'" . tuareg-mode))
+
+(add-hook 'tuareg-mode-hook 'rainbow-delimiters-mode)
+
+(map! :localleader
+      :map tuareg-mode-map
+      "j" #'merlin-jump
+      "l" #'merlin-locate
+      "p" #'merlin-pop-stack
+      "f" #'ocp-indent-buffer
+      )
