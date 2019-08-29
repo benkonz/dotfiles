d1039f5b334a80354c472ceb39428464650c869f
Ben Konz <benkonz16@gmail.com>
2019-08-29 14:00:32 -0400

    Update init.fish

--- init.fish
+++ init.fish
@@ -1,4 +1,21 @@
+# global variables
+set -xg PATH /Users/konz/.emacs.d/bin $PATH
+set -xg EDITOR emacs -nw
+set -xg VISUAL emacs
+set -xg CC clang
+set -xg CXX clang++
+
+# rust/cargo configuration
+source $HOME/.cargo/env
 set -xg RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
-set -xg EDITOR /usr/bin/EDITOR
-set -xg VISUAL /usr/bin/VISUAL
-eval (opam env)
+
+# fish autocomplete for GCP CLI utils
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+# opam configuration
+source /Users/konz/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# ammonite only works with bash. This wrapper functions inits amm with a bash shell
+function amm --description 'Scala REPL'
+    sh -c 'amm "$@"' amm $argv
+end
